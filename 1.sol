cffdcb6095f101c371515fc1b30b3789f4c20d94
Jalaram Thakkar <106419882+JalaramThakkar@users.noreply.github.com>
2022-06-02 08:19:45 +0530

    Create 1.sol

---
+++ 1.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+contract helloworld {
+    function printHelloWorld() public pure returns(string memory ){
+       return "hello world";
+
+
+    }//SPDX-license-identifier:GPL-3.0
+
+
+}
